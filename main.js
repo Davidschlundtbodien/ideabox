7a1a0e3a8d74f545df67591afb1ea3b6bb3e5678
William McGuire <wmcguire18@gmail.com>
2021-06-04 19:22:51 -0400

    Add helper function display gallery

--- main.js
+++ main.js
@@ -14,20 +14,27 @@ var titleInput = document.querySelector("#title-input").value;
 var bodyInput = document.querySelector("#body-input").value;
 var userIdea = new Idea(titleInput, bodyInput);
 userIdea.saveToStorage(ideas);
-ideaGallery.innerHTML +=
-`  <div class="idea-card">
-      <section class="idea-card-head">
-        <img src="assets/icons/star.svg" alt="star">
-        <img src="assets/icons/delete.svg" alt="delete">
-      </section>
-      <article class="idea-card-body">
-        <h4>${userIdea.title}</h4>
-        <p>${userIdea.body}</p>
-      </article>
-      <section class="idea-card-foot">
-        <img src="assets/icons/comment.svg" alt="comment">
-        <p>Comment</p>
-      </section>
-  </div>
-`
+displayGallery();
 };
+
+function displayGallery() {
+  ideaGallery.innerHTML = "";
+  for (var i = 0; i < ideas.length; i++) {
+    ideaGallery.innerHTML +=
+    `  <div class="idea-card">
+          <section class="idea-card-head">
+            <img src="assets/icons/star.svg" alt="star">
+            <img src="assets/icons/delete.svg" alt="delete">
+          </section>
+          <article class="idea-card-body">
+            <h4>${ideas[i].title}</h4>
+            <p>${ideas[i].body}</p>
+          </article>
+          <section class="idea-card-foot">
+            <img src="assets/icons/comment.svg" alt="comment">
+            <p>Comment</p>
+          </section>
+      </div>
+    `
+  }
+}
