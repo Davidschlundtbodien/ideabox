8ace4f1233069e08c566ae91e334bf2ee9524c14
Eric Sergeant <eric.sergeant.dev@gmail.com>
2021-06-05 12:56:17 -0500

    Complete delete functionality in iteration 3

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var ideaGallery = document.querySelector("#idea-gallery");
 var submitIdeaButton = document.querySelector("#submit-idea-button");
 var titleInput = document.querySelector("#title-input");
 var bodyInput = document.querySelector("#body-input");
-var ideaCard = document.querySelector(".idea-card");
+// var ideaCard = document.querySelector(".idea-card");
 
 //Event Listeners
 submitIdeaButton.addEventListener("click", createIdea);
@@ -29,35 +29,14 @@ function click(event) {
     var cardDate = Number(event.target.closest(".idea-card").id);
     for (var i = 0; i<ideas.length; i++) {
       if (ideas[i].id === cardDate) {
-      // ideaGallery.innerHTML -=
-      // `  <div class="idea-card" id="${ideas[i].id}">
-      //       <section class="idea-card-head">
-      //         <img src="assets/icons/star.svg" alt="star" class="favorite-card">
-      //         <img src="assets/icons/delete.svg" alt="delete" class="remove-card">
-      //       </section>
-      //       <article class="idea-card-body">
-      //         <h4>${ideas[i].title}</h4>
-      //         <p>${ideas[i].body}</p>
-      //       </article>
-      //       <section class="idea-card-foot">
-      //         <img src="assets/icons/comment.svg" alt="comment">
-      //         <p>Comment</p>
-      //       </section>
-      //   </div>
-      // `
-      ideas.splice(i, 1);
-      ideaCard.classList.add(".hidden");
-      console.log(cardDate);
+        ideas.splice(i, 1);
+        displayGallery();
+      }
     }
-  }
   console.log(ideas);
-}
+  }
 }
 
-
-// iterate through array
-// if card sellected by id matches id in array
-// perform delete function
 
 function displayGallery() {
   ideaGallery.innerHTML = "";
